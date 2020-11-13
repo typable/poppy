dc8cfc74df975e45def16e5107711adf7570fe88
luca-luca <reisingerluca@gmail.com>
2020-11-14 00:53:49 +0100

    Setup

---
+++ src/de/typable/minecrafthub/Main.java
@@ -0,0 +1,13 @@
+package de.typable.minecrafthub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		// TODO onEnable
+	}
+}
