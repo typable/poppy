98cbe37c6166dfdaa1aad2c1f4bdacdb71065bc1
Ubuntu <ubuntu@ubuntu.speedport.ip>
2023-01-12 23:26:43 +0000

    Removed hook file

--- hook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/andreas/git/poppy
-
-git --git-dir=../poppy/.git --work-tree=../poppy pull origin main
-
-/opt/gradle/gradle-7.6/bin/gradle build
-
-cp build/libs/poppy.jar /srv/minecraft/plugins/
